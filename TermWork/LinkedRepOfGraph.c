69e44abc5673b6b556054b38914d2fe0f75c0e6b
Abhinav Choudhary <abhinavchdhary@gmail.com>
2022-12-14 02:22:30 +0530

    linked rep and merge sort

---
+++ TermWork/LinkedRepOfGraph.c
@@ -0,0 +1,73 @@
+/*Write a C program to implement linked representation of a
+ graph in memory using array of 
+ pointers.*/
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct linkNode{
+    int dst;
+    struct linkNode* next;
+}linktype;
+typedef struct graphNode{
+    int data;
+    struct linkNode *head;
+}graphtype;
+
+//function to add values to graph
+void addNodes(graphtype* graph, int n){
+    printf("Enter data to be added:\n");
+    for(int i=0; i<n; i++){
+        scanf("%d", &((graph+i)->data));
+        (graph+i)->head = NULL;
+    }
+}
+//function to add new edges
+void addEdge(linktype** head, int x, int destination){
+    linktype* p= (linktype*)malloc(sizeof(linktype));
+    p->next = *head;
+    p->dst = destination;
+    *head = p;
+}
+//function to create edges from a node
+void createEdges(graphtype* graph, int n){
+    for(int i=0; i<n; i++){
+        int ch;
+        printf("Enter destinations for %d:\n", (graph+i)->data);
+        do{
+            printf("continue: 1/0:");
+            scanf("%d", &ch);
+            if(ch==1){
+                int destination;
+                scanf("%d", &destination);
+                addEdge(&((graph+i)->head), (graph+i)->data, destination);
+            }
+        }while(ch==1);
+    }
+}
+//function to display the graph
+void display(graphtype* graph, int n){
+    printf("The graph:\n");
+    for(int i=0; i<n; i++){
+        printf("%d", (graph+i)->data);
+        linktype* temp = (graph+i)->head;
+        while(temp!=NULL){
+            printf(" --> %d", temp->dst);
+            temp = temp->next;
+        }
+        printf("\n");
+    }
+}
+
+int main(){
+    int num;
+    printf("Enter the number of nodes: ");
+    scanf("%d", &num);
+    const int n = num;
+    graphtype* graph = (graphtype*)malloc(sizeof(graphtype)*n);
+
+    addNodes(graph, n);
+    createEdges(graph, n);
+    display(graph, n);
+
+    return 0;
+}
---
+++ TermWork/mergeSortChars.c
@@ -0,0 +1,14 @@
+/*Write a C program to sort an unsorted sequence of strings
+ given by user in an array, using 
+ Merge sort technique*/
+//CAUTION program is not complete!
+#define max 100
+#include <stdio.h>
+
+int main(){
+    char arr[max];
+    printf("Enter the sequence of characters\n");
+    fgets(arr, max, stdin);
+    //mergeSort(arr, 0, )
+    return 0;
+}
